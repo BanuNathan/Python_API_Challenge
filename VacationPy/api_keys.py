eb1549acf3a65bb1c66540c0abc448398120bd6d
Banu Nathan <banuswami1996@gmail.com>
2021-01-27 22:28:30 -0500

    commiting the hw files

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,3 @@
+api_key = "cc3dd93b32f9fff6ba1f8c252d9d7450"
+
+g_key = "AIzaSyCUm3WdywhRCi0JnTIH6JlIgnrC6-skuvs"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "cc3dd93b32f9fff6ba1f8c252d9d7450"
+
+# Google API Key
+g_key = "AIzaSyCUm3WdywhRCi0JnTIH6JlIgnrC6-skuvs"
